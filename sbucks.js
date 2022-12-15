8389bdddb1a239007edfe9cc0717cee1fe298c44
João Paulo <joaopaulosantosoliveira209@gmail.com>
2022-12-14 21:50:18 -0300

    fifth commit

---
+++ sbucks.js
@@ -0,0 +1,9 @@
+function imgSlider (anything){
+    document.querySelector ('.starbucks').src=anything
+
+}
+
+function circleColor(color){
+    const circle = document.querySelector('.circle')
+    circle.style.background = color;
+}

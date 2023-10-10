ed4bde7945577741a91d117a39a0f945f52b744e
João Oliveira <joaopaulosantosoliveira209@gmail.com>
2023-10-09 21:31:49 -0300

    tornei a pagina responsiva

--- JS/sbucks.js
+++ JS/sbucks.js
@@ -7,3 +7,9 @@ function circleColor(color){
     const circle = document.querySelector('.circle')
     circle.style.background = color;
 }
+
+
+//Menu mobile
+
+
+let btn_menu_mobile = document.querySelector('.menu-button')
